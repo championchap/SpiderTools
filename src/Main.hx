4072b8b3038484f2de13cd902408c84e1fbff819
Erik Watson <championchap@gmail.com>
2015-04-29 04:53:17 +0100

    working on an unzip function

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,9 @@
 package ;
 
+import sys.io.File;
+import haxe.zip.Reader;
+import sys.FileSystem;
+
 class Main {
 
     static function main() {
@@ -26,7 +30,9 @@ class Main {
         // check the name is valid
         // create the folder if it doesn't already exist
         // unzip and copy the empty project template files into the new folder
-        // done! :D 
+        // done! :D
+
+        unzip("/home/erik/Documents/Code/Apps/SpiderTools/templates/test.zip", '${name}');
     }
 
     private function lost() {
@@ -35,6 +41,36 @@ class Main {
 
     private function help() {
         Sys.println("To create a new project, use -n \"My Project Name\" inside the directory that you want to create the project folder in.");
+    }
+
+    private function unzip(zip:String, dest:String) {
+        var f = File.read(zip);
+        var enries = Reader.readZip(f);
+
+        f.close();
+
+        var cwd = Sys.getCwd();
+
+        for(e in enries){
+            var fileName = e.fileName;
+            var dirs = fileName.split("/");
+
+            // no dirs
+            if(dirs.length == 1) {
+                dirs = [];
+            } else {
+                dirs.pop();
+            }
+
+            for(dir in dirs){
+                var dirPath = '${cwd}${dest}/${dir}';
+
+                trace(dirPath);
+            }
+
+            // create the directories it lives in
+            // write the file 
+        }
     }
 
 }
