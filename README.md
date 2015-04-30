ff4f934502ea6c314f5ccb01e58033d864bd9ad3
Erik Watson <championchap@gmail.com>
2015-04-30 16:02:27 +0100

    getting started on vhosts, edited the readme too

--- README.md
+++ README.md
@@ -12,6 +12,18 @@ To make a new blank project called HelloWorld in this directory, just do this.
 spider-tools -n "HelloWorld"
 ```
 
+If you want Spider Tools to set up a vhost for you, just use -v.
+
+Spider assumes that the path you want to use is the default public path for a new Spider Project, but local to the location you are in when you run the command. For this reason, it is best to use this command from inside your spider project folder.
+
+If you want to change the path it points to, pass it in a second parameter. 
+
+Currently, this is only being built to work with XAMPP on Linux & OS X. 
+
+```
+spider-tools -v "spider.dev" "set/public_path/"
+```
+
 
 ## License
 
--- src/Main.hx
+++ src/Main.hx
@@ -7,115 +7,147 @@ import haxe.zip.Reader;
 
 class Main {
 
-    static function main() {
-        new Main();
-    }
+	private var isBSD(get, null):Bool;
+	private var isLinux(get, null):Bool;
+	private var isMac(get, null):Bool;
+	private var isWindows(get, null):Bool;
 
-    public function new() {
-        var args = Sys.args();
+	static function main() {
+		new Main();
+	}
 
-        switch(args[0]) {
-            case "-n":
-                // do thing
-                newProject(args[1]);
+	public function new() {
+		var args = Sys.args();
 
-            case "-v":
-                newVHost(args[1]);
+		switch(args[0]) {
+			case "-n":
+				// do thing
+				newProject(args[1]);
 
-            case "-h":
-                help();
+			case "-v":
+				newVHost(args[1]);
 
-            default:
-                lost();
-        }
-    }
+			case "-h":
+				help();
 
-    private function newProject(name:String) {
-        // TODO: Use a relative path once we know how this will be installed 
-        unzip("/home/erik/Documents/Code/Apps/SpiderTools/templates/NewProject.zip", '${name}');
-    }
+			default:
+				lost();
+		}
+	}
 
-    private function newVHost(name:String) {
-        // TODO: This might not be easy
-        // For now this will work with only xampp, and only on Mac or Linux. 
-        Sys.println('Quickly adds ${name} as a new vhost to your system.');
-    }
+	private function newProject(name:String) {
+		// TODO: Use a relative path once we know how this will be installed 
+		unzip("/home/erik/Documents/Code/Apps/SpiderTools/templates/NewProject.zip", '${name}');
+	}
 
-    private function lost() {
-        Sys.println("If you get stuck, use -h to read the help file");
-    }
+	private function newVHost(name:String, path:String = '') {
+		var httpd;
+		var httpd_location = '';
 
-    private function help() {
-        Sys.println("To create a new project, use -n \"My Project Name\" inside the directory that you want to create the project folder in.");
-        Sys.println("To create a new vhost use -v \"example.dev\"");
-    }
+		var hosts;
+		var hosts_location = '';
 
-    private function unzip(zip:String, dest:String) {
-        var f = File.read(zip);
-        var entries = Reader.readZip(f);
+		var vhosts;
+		var vhosts_location = '';
 
-        f.close();
+		if(isLinux) {
+			httpd_location = '/opt/lampp/etc/httpd.conf';
+			hosts_location = '/etc/hosts';
+			vhosts_location = '/opt/lampp/etc/extra/httpd-vhosts.conf';
+		}
 
-        var cwd = Sys.getCwd();
+		if(isMac) {  }
+		if(isWindows) {  }
+		if(isBSD) {  }
 
-        for(e in entries){
-            var fileName = e.fileName;
-            var dirs = fileName.split("/");
-            var filename = "";
-            var path = '${dest}/';
+		// search hosts for the name
+		// search the vhosts file for the name 
 
-            filename = dirs.pop();
+		// if we don't find it in either of those, move on
 
-            for(dir in dirs){
-                var dirPath = '${dest}/${dir}/';
-                path += '${dir}/';
-            }
+		// make a backup copy of these files
 
-            if(!FileSystem.exists(path)) {
-                FileSystem.createDirectory(path);
-                Sys.println('Spinning : ${path}');
-            }
+		// insert the name + ip into the hosts file 
+		// insert the vhost into the vhosts file
 
-            if(filename != "") {
-                var data = Reader.unzip(e);
-                var output = File.write('${path}${filename}');
+		// close the files 
+	}
 
-                output.close();
-            }
+	private function lost() {
+		Sys.println("If you get stuck, use -h to read the help file");
+	}
 
-        }
-    }
+	private function help() {
+		Sys.println("To create a new project, use -n \"My Project Name\" inside the directory that you want to create the project folder in.");
+		Sys.println("To create a new vhost use -v \"example.dev\"");
+	}
 
-    private function isWindows() {
-        if(Sys.systemName() == "Windows") {
-            return true;
-        }
-        
-        return false;
-    }
+	private function unzip(zip:String, dest:String) {
+		var f = File.read(zip);
+		var entries = Reader.readZip(f);
 
-    private function isMac() {
-        if(Sys.systemName() == "Mac") {
-            return true;
-        }
-        
-        return false;
-    }
+		f.close();
 
-    private function isLinux() {
-        if(Sys.systemName() == "Linux") {
-            return true;
-        }
-        
-        return false;
-    }
+		var cwd = Sys.getCwd();
 
-    private function isBSD() {
-        if(Sys.systemName() == "BSD") {
-            return true;
-        }
-        
-        return false;
-    }
+		for(e in entries){
+			var fileName = e.fileName;
+			var dirs = fileName.split("/");
+			var filename = "";
+			var path = '${dest}/';
+
+			filename = dirs.pop();
+
+			for(dir in dirs){
+				var dirPath = '${dest}/${dir}/';
+				path += '${dir}/';
+			}
+
+			if(!FileSystem.exists(path)) {
+				FileSystem.createDirectory(path);
+				Sys.println('Spinning : ${path}');
+			}
+
+			if(filename != "") {
+				var data = Reader.unzip(e);
+				var output = File.write('${path}${filename}');
+
+				output.close();
+			}
+
+		}
+	}
+
+	private function get_isWindows() {
+		if(Sys.systemName() == "Windows") {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private function get_isMac() {
+		if(Sys.systemName() == "Mac") {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private function get_isLinux() {
+		if(Sys.systemName() == "Linux") {
+			return true;
+		}
+		
+		return false;
+	}
+
+	private function get_isBSD() {
+		if(Sys.systemName() == "BSD") {
+			return true;
+		}
+		
+		return false;
+	}
 
 }
